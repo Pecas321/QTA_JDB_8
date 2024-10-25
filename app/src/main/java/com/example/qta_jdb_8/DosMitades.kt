4724fa967a960b81a781c6bad633f8352c20b0e3
Pecas321 <danielc.salinasr@utadeo.edu.co>
2024-10-24 20:12:26 -0500

    Commit Dani 4

---
+++ app/src/main/java/com/example/qta_jdb_8/DosMitades.kt
@@ -0,0 +1,15 @@
+package com.example.qta_jdb_8
+
+import android.os.Bundle
+import android.text.Html
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class DosMitades : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dos_mitades)
+
+
+    }
+}
